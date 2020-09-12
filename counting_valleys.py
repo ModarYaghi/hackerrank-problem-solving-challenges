573ceaf33ab3d1c08c10ce76df942ec6ae89ad92
ModarYaghi <mfyaghi81@gmail.com>
2020-09-13 01:21:24 +0300

    Python solution for Counting Valleys challenge

---
+++ counting_valleys.py
@@ -0,0 +1,21 @@
+"""Hacker Rank Problem Solving - Challenge 23; Counting Valleys"""
+
+
+def counting_valleys(n, s):
+    sea_surface = 0
+    valleys = 0
+
+    for step in s:
+        if step == "U":
+            sea_surface += 1
+            if sea_surface == 0:
+                valleys += 1
+
+        else:
+            sea_surface += -1
+
+    return valleys
+
+
+print(counting_valleys(8, "UDDDUDUU"))  # 1
+print(counting_valleys(12, "DDUUDDUDUUUD"))  # 2
