11aef45a68b69dcbf7caed17a42f957f542f2a51
ModarYaghi <mfyaghi81@gmail.com>
2020-09-13 23:58:31 +0300

    Python solution for Solve Me First Challenge

---
+++ solve_me_first.py
@@ -0,0 +1,19 @@
+"""Hacker Rank Problem Solving - Challenge 01; Solve Me First"""
+
+# This challenge isn't explained well at the hackerrank site,
+# but I think this solution is the simplest one to get the challenge passed.
+
+
+def solve_me_first(a, b):
+    return a + b
+
+
+a = int(input())
+b = int(input())
+
+print(solve_me_first(a, b))
+
+
+# The test instances
+# a = 2, b = 3, output = 5
+# a = 100, b = 1000, output = 1100
