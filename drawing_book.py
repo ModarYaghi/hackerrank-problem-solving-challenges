6613f63bba18ff8b3229cbd8360573b6038f0957
ModarYaghi <mfyaghi81@gmail.com>
2020-09-14 00:07:22 +0300

    Python solution for Drawing Book Challenge

--- drawing_book.py
+++ drawing_book.py
@@ -5,7 +5,10 @@ def page_count(n, p):
     return min(p // 2, n // 2 - p // 2)
 
 
-print(page_count(5, 4))
-print(page_count(6, 2))
-print(page_count(201, 79))
-print(page_count(552, 401))
+# The primary test instances
+print(page_count(5, 4))  # 0
+print(page_count(6, 2))  # 1
+
+# More instances
+print(page_count(201, 79))  # 39
+print(page_count(552, 401))  # 76
