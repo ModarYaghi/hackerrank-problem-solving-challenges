76107990c86bd7cc1ba35f1bf1cfd4cf720ece29
ModarYaghi <mfyaghi81@gmail.com>
2020-09-14 01:55:30 +0300

    Python solution for Simple Array Sum

---
+++ simple_array_sum.py
@@ -0,0 +1,9 @@
+"""Hacker Rank Problem Solving - Challenge 02; Simple Array Sum"""
+
+
+def simple_array_sum(ar):
+    return sum(ar)
+
+
+# The primary test instance
+print(simple_array_sum([1, 2, 3, 4, 10, 11]))  # 31
