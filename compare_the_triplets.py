a59b6f8d448f182b3c58fc63a5307b747cd752f8
ModarYaghi <mfyaghi81@gmail.com>
2020-09-19 02:11:38 +0300

    Python solution for Compare The Triplets challenge

---
+++ compare_the_triplets.py
@@ -0,0 +1,24 @@
+"""Hacker Rank Problem Solving - Challenge 03; Compare The Triplets"""
+
+
+def compare_triplets(a, b):
+    scores = [0, 0]
+
+    for i in range(3):
+        if a[i] > b[i]:
+            scores[0] += 1
+        elif a[i] < b[i]:
+            scores[1] += 1
+
+    return scores
+
+
+# The primary test instances:
+a, b = [1, 2, 3], [3, 2, 1]
+print(compare_triplets(a, b))  # [1, 1]
+
+a, b = [5, 6, 7], [3, 6, 10]
+print(compare_triplets(a, b))  # [1, 1]
+
+a, b = [17, 28, 30], [99, 16, 8]
+print(compare_triplets(a, b))  # [2, 1]
