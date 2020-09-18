6e54bbaa1c70ea4882dd550191e15e97a49f8d83
ModarYaghi <mfyaghi81@gmail.com>
2020-09-19 02:28:34 +0300

    A Very Big Sum challenge

---
+++ a_very_big_sum.py
@@ -0,0 +1,10 @@
+"""Hacker Rank Problem Solving - Challenge 04; A Very Big Sum"""
+
+
+def a_very_big_sum(ar):
+    return sum(ar)
+
+
+# The primary test instance:
+ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+print(a_very_big_sum(ar))  # 5000000015
